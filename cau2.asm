8fb1c0071120e552f0a267a3a087022f3e825611
nicejjss <77496505+nicejjss@users.noreply.github.com>
2023-11-05 18:01:47 +0700

    Update cau2.asm

--- cau2.asm
+++ cau2.asm
@@ -17,31 +17,31 @@ PUBLIC @trungbinhcong$qv
 	mov  ds,ax
     PS:
 	clrscr
-	HienString M1	; Hiện thông báo M1 (‘Hay vao so thu 1 : ‘)
-	call  VAO_SO_N	; Nhận giá trị số thứ 1
-	mov  bx,ax		; bx = giá trị số thứ 1
-	HienString M2	; Hiện thông báo M2 (‘Hay vao so thu 2 : ‘)
-	call  VAO_SO_N	; Nhận giá trị số thứ 2
-	HienString M3	; Hiện thông báo M3 (‘Trung binh cong 2 so nguyen la :’)
-	add  ax,bx		; Tổng 2 số (ax=ax+bx)
-	and  ax,ax		; Giá trị tổng là âm hay dương?
-	jns   L1		; Tổng là dương thì nhảy đến L1
-	HienString dautru	; còn âm thì hiện dấu ‘-‘
-	neg  ax		; và đổi dấu số bị chia
+	HienString M1	
+	call  VAO_SO_N	
+	mov  bx,ax		
+	HienString M2	
+	call  VAO_SO_N	
+	HienString M3	
+	add  ax,bx		
+	and  ax,ax		
+	jns   L1		
+	HienString dautru	
+	neg  ax		
     L1:
-	shr   ax,1		; Chia đôi làm tròn dưới
-	pushf			; Cất giá trị cờ vào stack (thực chất là giá trị cờ Carry)
-	call  HIEN_SO_N	; Hiện giá trị trung bình cộng làm tròn dưới
-	popf			; Lấy lại giá trị cờ từ stack (lấy lại trạng thái bit cờ Carry)
-	jnc   L2		; Nếu Carry=0 (giá trị tổng là chẳn) thì nhảy
-	HienString M4	; còn  Carry # 0 thì hiện thêm ‘.5’ lên màn hình
+	shr   ax,1		
+	pushf			
+	call  HIEN_SO_N	
+	popf			
+	jnc   L2		
+	HienString M4	
      L2:
-	HienString M5	; Hiện thông báo M5 (‘Co tiep tuc CT (c/k)? ‘)
-	mov  ah,1		; Chờ nhận 1 ký tự từ bàn phím
+	HienString M5	
+	mov  ah,1		
 	int     21h
-	cmp  al,'c'		; Ký tự vừa nhận có phải là ký tự ‘c’ ?
-	jne    Exit		; Nếu không phải thì nhảy đến nhãn Exit (về DOS)
-	jmp   PS		; Còn không thì quay về đầu (bắt đầu lại chương trình)
+	cmp  al,'c'		
+	jne    Exit		
+	jmp   PS		
    Exit:
 	 ret
 @trungbinhcong$qv ENDP
