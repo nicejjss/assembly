7703b402fcf511431eecacb090aa69003772c408
nicejjss <77496505+nicejjss@users.noreply.github.com>
2023-11-07 00:08:02 +0700

    Update cau1.asm

--- cau1.asm
+++ cau1.asm
@@ -1,8 +1,10 @@
 INCLUDE lib1.asm
 .MODEL small
 .DATA
-dtt1  db 13,10,'   Hay vao ten tep can dat thuoc tinh: $'
-dtt2  db 13,10,'          Vao thuoc tinh cho tep: '
+dtt1  db 13,10,'        CHUC NANG DAT THUOC TINH CHO TEP'
+      db 13,10,'        --------------oOo---------------'
+      db 13,10,13,10,'     Hay vao ten tep can dat thuoc tinh: $'
+dtt2  db 13,10,'     Vao thuoc tinh cho tep: '
         db 13,10,'          0. Khong dat thuoc tinh'
         db 13,10,'          1. Read Only'
         db 13,10,'          2. Hidden'
@@ -20,47 +22,45 @@ dtt2  db 13,10,'          Vao thuoc tinh cho tep: '
         db 13,10,'         14. Archive+Hidden+System'
         db 13,10,'         15. Archive+Read Only+Hidden+System'
         db 13,10,13,10,'        -> Hay chon: $'
-  Err_DTT db 13,10,'   Khong dat duoc thuoc tinh cho tep! $'
-  Suc_DTT db 13,10,'   Tep da dat duoc thuoc tinh! $'
+  Err_DTT db 13,10,'     Khong dat duoc thuoc tinh cho tep! $'
+  Suc_DTT db 13,10,'     Tep da dat duoc thuoc tinh! $'
   buff          db 30
-                   db ?
+                db ?
   file_name db 30 dup(?)
-	  tieptuc      db 13,10,' Co tiep tuc CT (c/k) ? $'
+  tieptuc      db 13,10,'     Co tiep tuc CT (c/k) ? $'
 .CODE
 PUBLIC @setthuoctinh$qv
 @setthuoctinh$qv PROC
-		mov  ax,@data
-mov  ds,ax
    L_DTT0:
-           clrscr
-           HienString dtt1		
+           clrscr                 ; Xoa man hinh
+           HienString dtt1        ; Hien thong bao dtt1
            lea    dx,buff
-           call   GET_FILE_NAME 
-			HienString dtt2	
-			call  VAO_SO_N     
-			mov   cx,ax		
-			cmp   cx,8
-			jb       L_DTT1
-	add    cx,24	
-  L_DTT1:	
-	lea     dx,file_name	
-	mov   al,1		
-	mov  ah,43h
-	int    21h
-	jnc   L_DTT2
-	HienString Err_DTT
-	jmp  Exit_DTT
-    L_DTT2:
-	HienString Suc_DTT
-    Exit_DTT:
-          HienString tieptuc      
-          mov  ah,1                   
-          int     21h                         
-          cmp   al,'c'                
-          jne    Thoat_DTT      
-          jmp    L_DTT0	
-   Thoat_DTT:                     
-			ret
+           call   GET_FILE_NAME   ; Vao ten tep can dat thuoc tinh 
+           HienString dtt2        ; Hien thong bao dtt2
+           call   VAO_SO_N        ; Vao thuoc tinh
+           mov    cx,ax           ; Dua thuoc tinh - > CX
+           cmp    cx,8            ; Lieu thuoc tinh tu 0 den 7 hay tren?
+           jb     L_DTT1          ; Thuoc tinh vao duoi 8 thi nhay den L_DTT1,
+           add    cx,24           ; con thuoc tinh vao tu 8 tro len thi xac lap TT la CX+4
+   L_DTT1: 
+           lea    dx,file_name    ; DX tro den dau xau ten tep
+           mov    al,1            ; Dat thuoc tinh
+           mov    ah,43h          ; Chuc nang dat thuoc tinh cho tep
+           int    21h
+           jnc    L_DTT2          ; CF=0 ... Dat thuoc tinh thnh cong thi nhay,
+           HienString Err_DTT     ; con CF=1 ... Thong bao loi
+           jmp    Exit_DTT
+   L_DTT2:
+           HienString Suc_DTT     ; Hien thong bao dat thuoc tinh thanh cong
+  Exit_DTT:
+           HienString tieptuc     ; Hien thong bao tiep tuc 
+           mov    ah,1            ; Cho nhan 1 ky tu tu ban phim       
+           int    21h                         
+           cmp    al,'c'          ; Lieu ky tu nhan la 'c'?      
+           jne    Thoat_DTT       ; Khong phai la 'c' thi nhay ve Thoat_DTT,
+           jmp    L_DTT0          ; con la 'c' thi tro ve L_DTT0
+  Thoat_DTT:                     
+           ret
 @setthuoctinh$qv ENDP
 INCLUDE lib2.asm		
 INCLUDE lib4.asm		
