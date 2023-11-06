1ad0be826be48e799c1e5e25fb3e049f646578ee
nicejjss <77496505+nicejjss@users.noreply.github.com>
2023-11-07 00:08:50 +0700

    Update cau3.asm

--- cau3.asm
+++ cau3.asm
@@ -1,32 +1,29 @@
 INCLUDE lib1.asm
 .MODEL small
 .DATA
-    m1 db 13,10,"May tinh co FPU khong?: $"
-    co db " Co$"
-    ko db " Ko$"
-	tieptuc db 13,10,' Co tiep tuc CT (c/k)?: $'
+ m1 db 13,10,'        CHUC NANG THONG TIN VE FPU'
+    db 13,10,'        ------------oOo-----------'
+    db 13,10,13,10,'     May tinh co FPU khong?: $'
+ co  db 'Co$'
+ ko  db 'Khong$'
+ tieptuc db 13,10,'     An phim bat ky de ve man hinh chuc nang $'
 .CODE
 PUBLIC @FPU$qv
 @FPU$qv PROC
-		mov  ax,@data
-        mov  ds,ax
     L_DTT0:
-        clrscr
-        HienString m1
-        int 11h
-        shr al,2
-        jc L_DTT1
-        HienString ko
-    L_DTT1:
-        HienString co
+        clrscr                  ; Xoa man hinh
+        HienString m1           ; Hien thong bao m1
+        int 11h                 ; 0:410h -> AL va 0:411h -> AH
+        shr al,2                ; Dich phai 2 lan (dua trang thai FPU -> CF)
+        jc  L_DTT1              ; Neu CF=1 (Co FPU) thi nhay,
+        HienString ko           ; con CF=0 thi Hien ko
+    L_DTT1:                                    
+        HienString co           ; Hien co
     L_DTT2:
-          HienString tieptuc	
+        HienString tieptuc 
 	mov  ah,1		
-	int     21h
-	cmp  al,'c'		
-	jne    Exit		
-	jmp   L_DTT0
+        int  21h
    Exit:
-	 ret
+        ret
 @FPU$qv ENDP
 END
